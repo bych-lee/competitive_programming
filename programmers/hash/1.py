0082fb350a8bfe48e62065ac70c375907d01dcad
Byeongchan Lee <prinwinter@gmail.com>
2022-06-26 21:03:53 +0900

    Update 1.py

--- programmers/hash/1.py
+++ programmers/hash/1.py
@@ -1,1 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42576?language=python3
 
+from collections import Counter
+
+def solution(participant, completion):
+    for p in participant:
+        if p not in completion:
+            return p
+        else:
+            completion.remove(p)
