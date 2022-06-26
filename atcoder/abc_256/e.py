5598d2a07376b81ac478ccd550b304ee640ad007
Byeongchan Lee <prinwinter@gmail.com>
2022-06-26 20:58:23 +0900

    Create e.py

---
+++ atcoder/abc_256/e.py
@@ -0,0 +1,20 @@
+N = int(input())
+X = list(map(int, input().split()))
+C = list(map(int, input().split()))
+
+is_visited = [False] * N
+frustration = 0
+
+for n in range(N):
+    graph = []
+    while not is_visited[n]:
+        is_visited[n] = True
+        graph.append(n)
+        n = X[n] - 1
+    for i in range(len(graph)):
+        if graph[i] == n:
+            cycle = graph[i:]
+            frustration += min([C[x] for x in cycle])
+            break
+
+print(frustration)
